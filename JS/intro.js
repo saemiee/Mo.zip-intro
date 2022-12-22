c12ad76c52f87436e43abe3ccba10ea255d16dbb
새미 <s22058@gsm.hs.kr>
2022-12-22 09:39:02 +0900

    card 애니메이션

---
+++ JS/intro.js
@@ -0,0 +1,28 @@
+const bgColors = ['#fff', '#fff', '#fff']
+const gropElems = document.querySelectorAll('.grop')
+const partnerElems = document.querySelectorAll('.partner');
+
+const setColorOfSection = () => {
+  gropElems.forEach((gropElems, idx) => {
+    gropElem.style.backgroundColors[idx]
+  })
+}
+
+// IntersectionObserver 등록하기
+const i = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    console.log(entry)
+    if(entry.isIntersecting) {
+      entry.target.classList.add('active')
+    } else {
+      entry.target.classList.remove('active')
+    }
+  })
+})
+
+// const contentElems = document.querySelectorAll('.partner');
+
+partnerElems.forEach((el) => {
+  i.observe(el);
+})
+
